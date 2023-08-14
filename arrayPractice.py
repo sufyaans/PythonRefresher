968f728c9fc95c7dba1524e8ff861a04cd569ee7
sufyaan434 <sufyaanseedat434@gmail.com>
2023-08-14 11:09:00 +0100

    Initial 2

--- arrayPractice.py
+++ arrayPractice.py
@@ -1,7 +1,6 @@
+num = [2,3,4,5,5,6, 7]
+
 #Write a Python program to calculate the sum of all elements in an array.
-
-num = [2,3,4,5,6]
-
 def calcSum():
     #num = [2,3,4,5,6]
     sum = 0
@@ -53,4 +52,98 @@ def arraySearch():
     else:
         print(f"{val} not found in the list")
 
-arraySearch()
+#Write a program that takes an array of numbers as input and removes any duplicate elements from the array.
+def duplicates():
+   
+    new_list = set(num)
+    print(list(new_list))
+    #Finds duplicates
+    # for i in range(len(num)):
+    #     for j in range (i+1,len(num)):
+    #         if num[i] == num[j]:
+    #             num[j]
+
+
+#Write a program that sorts the elements of an array in ascending order.
+def sortArray():
+    newList = [int(i) for i in num]
+    newList.sort(reverse=True)
+    print(newList)
+
+#Write a program that takes two arrays as input and prints the elements that are common to both arrays.
+def commonAttr():
+    num2 = [2,8,1,3,7,0]
+    sim = []
+    for i in range(len(num)):
+        for j in range(len(num2)):
+            if num[i] == num2[j]:
+               
+                sim.append(num2[j])
+    print(sim)
+   
+#Write a program that takes two arrays as input and prints the union of the two arrays (all unique elements from both arrays).
+def diffAttr():
+    num2 = [2,8,1,3,7,0]
+    diff = list(set(num).symmetric_difference(num2))
+    print(diff)
+
+
+
+#Write a program that calculates and prints the average of the numbers in an array.
+def findavg():
+    x = sum(num)
+    avg = x/len(num)
+    print(avg)
+
+#Implement a program that takes a sentence as input and returns the frequency of each word in the sentence using a dictionary.
+sentence = "Hello how are you you look amazing are you good"
+def freq():
+    li = sentence.split()
+
+    wordCount = dict()
+    for i in li:
+        if i in wordCount:
+            wordCount[i] = wordCount[i] + 1
+        else:
+            wordCount[i] = 1
+    print(wordCount)
+   
+
+#Implement a function that checks if a given array is a palindrome (reads the same forwards and backwards).
+def palindrome():
+    word = "racecar"
+    
+    wordRev = word[::-1]
+    if (word!= wordRev):
+        print(False)
+    else:
+        print(True)
+
+#Implement a recursive function to calculate the nth Fibonacci number.
+
+def Fibonacci(n):
+    if n<= 0:
+        print("Incorrect input")
+    # First Fibonacci number is 0
+    elif n == 1:
+        return 0
+    # Second Fibonacci number is 1
+    elif n == 2:
+        return 1
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
+    
+def fibs(n):
+    #n = 20
+    if n<= 0:
+        print("Incorrect input")
+    elif n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return fibs(n-1) + fibs(n-2)
+    
+print(fibs(10))
+
+ 
---
+++ test.py
@@ -0,0 +1,44 @@
+
+'''
+Create a class named Person with attributes "name" and "age". 
+Define a constructor to initialize these attributes and a method called "introduce" that prints out a message introducing the person. 
+Create an instance of the class and call the "introduce" method.
+'''
+# class Person:
+
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def introduce(self):
+#         print(f"Hello my name is {self.name}, and I am {self.age} years old")
+
+
+# p1 = Person("Sufyaan", 23)
+# p1.introduce()
+
+
+'''
+Create a class named "Rectangle" with attributes "width" and "height". 
+Implement methods to calculate the area and perimeter of the rectangle. 
+Create an instance of the class, set the width and height, 
+and then print out its area and perimeter.
+'''
+# class Rectangle:
+#     def __init__(self,width,height):
+#         self.width = width
+#         self.height = height
+
+#     def calc(self):
+#         total = self.width * self.height
+#         return total
+    
+
+# rec1 = Rectangle(10,15)
+# print(rec1.calc())
+
+'''
+Create a class named Student with attributes name, age, and grade. 
+Implement a method called display_info that prints out the student's name, age, and grade. 
+Create multiple instances of the class with different information and call the display_info method for each instance.
+'''
