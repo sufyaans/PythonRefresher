866a03c2cda9bc0debbe272a65b48987d3ba72b7
sufyaan434 <sufyaanseedat434@gmail.com>
2023-08-15 15:53:58 +0100

    employeeSystem question

---
+++ employeeSystem.py
@@ -0,0 +1,32 @@
+'''
+Create a class called Employee with attributes like name, age, and salary. 
+Implement methods for salary calculation and printing employee information. 
+Then, create subclasses for different types of employees like Manager, Developer, and Designer.
+'''
+
+class Employee:
+    
+    def __init__(self, name, age, salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+    
+    def salary_(self, position):
+        multiplier = 2 
+        self.position = position
+
+        if position == "Manager":
+            multiplier = 3
+        elif position == "Developer":
+            multiplier = 6
+        elif position == "Designer":
+            multiplier = 9
+
+        annualSalary = self.salary * multiplier        
+        return annualSalary
+    
+
+class Manager(Employee):
+    def __init__(self):
+        pass
+
